cc45cbd7472d440d5584361df365b2b52fdc9983
Vladislav Sergeev <vladismann@yandex.ru>
2023-04-05 17:30:11 +0300

    feat: черновик контроллеры

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final HashMap<int, User> users = new HashMap<>();
+
+    @RequestMapping
+
+    @GetMapping()
+    public Collection<User> getAll() {
+        return users.values();
+    }
+
+    @PostMapping()
+    public User create(@RequestBody User user) throws ValidationException {
+        if (users.containsKey(user.getId())) {
+            throw new ValidationException(user.getEmail() + " уже недоступен");
+        } else if (user.getEmail() == null || user.getEmail().isEmpty() || user.getEmail().isBlank()) {
+            throw new ValidationException("Укажите корректный email");
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping()
+    public User update(@RequestBody User user) throws ValidationException {
+        if (user.getEmail() == null || user.getEmail().isEmpty() || user.getEmail().isBlank()) {
+            if (!users.containsKey(user.getId())) {
+                throw new ValidationException("Укажите корректный email");
+            }
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+@Data
+public class Film {
+
+    private int id;
+    private String name;
+    private LocalDate releaseDate;
+    private Duration duration;
+}
