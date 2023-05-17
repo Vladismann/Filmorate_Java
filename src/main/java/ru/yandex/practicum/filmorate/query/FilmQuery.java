ac28f30b1db5ed25b865173cbe0025bc5d2b2a3c
Vlad <vladismann@yandex.ru>
2023-05-17 22:11:38 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/query/FilmQuery.java
+++ src/main/java/ru/yandex/practicum/filmorate/query/FilmQuery.java
@@ -20,6 +20,7 @@ public class FilmQuery {
                 "INNER JOIN genres g ON fg.genre_id = g.genre_id " +
                 "WHERE fg.film_id = " + filmId;
     }
+
     public static final String DELETE_OLD_GENRES = "DELETE FROM film_genres WHERE film_id = ?";
     public static final String UPDATE_FILM = "UPDATE films SET name = ?, description = ?, release_date = ?, duration = ?, rating_id = ? WHERE film_id = ?";
     public static final String ADD_LIKE = "INSERT INTO film_likes " +
@@ -27,14 +28,16 @@ public class FilmQuery {
             "VALUES (?, ?)";
     public static final String CHECK_LIKE = "SELECT * FROM film_likes WHERE film_id = ? AND user_id = ?";
     public static final String DELETE_LIKE = "DELETE FROM film_likes WHERE film_id = ? AND user_id = ?";
+
     public static String getPopularFilmsQuery(int count) {
-         return "SELECT f.*, r.*, COUNT(fl.film_id) as likes " +
+        return "SELECT f.*, r.*, COUNT(fl.film_id) as likes " +
                 "FROM films f " +
                 "INNER JOIN rating_mpa r ON f.rating_id = r.rating_id " +
                 "LEFT JOIN film_likes fl ON f.film_id = fl.film_id " +
                 "GROUP BY f.film_id " +
                 "ORDER BY likes DESC LIMIT " + count;
     }
+
     public static final String GET_ALL_GENRES_QUERY = "SELECT * FROM genres";
     public static final String GET_GENRE_BY_ID = "SELECT * FROM genres WHERE genre_id = ?";
     public static final String GET_ALL_MPA = "SELECT * FROM rating_mpa";
